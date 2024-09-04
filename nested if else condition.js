58b10d7d7c0804a792af1a0241a9c1bd283a51ac
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-04 20:21:20 +0600

    first commit

---
+++ nested if else condition.js
@@ -0,0 +1,18 @@
+const money = 102;
+
+if(money > 500){
+    console.log('lets watch movie');
+}
+else{
+    if(money > 100){
+        console.log('lets drink coffee')
+    }
+    else{
+        if(money >50){
+            console.log('we will eat food')
+        }
+        else{
+            console.log('Back home')
+        }
+    }
+}
